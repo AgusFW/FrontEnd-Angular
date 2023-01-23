1d49a657dd234081c5c750e66eb3b5bb604884b8
AgusFW <agustina.witis@gmail.com>
2023-01-22 22:10:35 -0300

    last commit

--- src/app/componentes/presentacion/presentacion.component.ts
+++ src/app/componentes/presentacion/presentacion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from 'src/app/servicios/portfolio.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-presentacion',
@@ -8,7 +9,20 @@ import { PortfolioService } from 'src/app/servicios/portfolio.service';
 })
 export class PresentacionComponent implements OnInit {
   miPortfolio:any;
-  constructor(private datosPortfolio:PortfolioService) { }
+  form: FormGroup;
+  presEdit: any;
+
+
+  constructor(private datosPortfolio:PortfolioService,  private formbuilder: FormBuilder) { 
+    this.form = this.formbuilder.group({
+
+      id:["",Validators.required],
+      nombre:["", Validators.required],
+      sobre_mi:["", Validators.required],
+      motivo:["", Validators.required],
+      img_persona: ["", Validators.required]
+   })
+  }
 
   ngOnInit(): void {
     this.datosPortfolio.obtenerDatos().subscribe(data =>{
@@ -16,5 +30,26 @@ export class PresentacionComponent implements OnInit {
       this.miPortfolio=data;
     });
   }
+
+ verPres(presEdit: any): void{
+    this.datosPortfolio.buscarPres(presEdit.id).subscribe(data =>{    
+      this.form.patchValue({
+        id: data.id,
+        nombre: data.nombre,
+        sobre_mi: data.sobre_mi,
+        motivo: data.motivo,
+        img_persona: data.img_persona,
+      })
+      this.presEdit=data; 
+      console.log(data); 
+    }); 
+  }
+
+  modPres(){
+    this.datosPortfolio.modificarPres(this.form.value).subscribe(data =>{
+    console.log(data);
+    this.presEdit=data;
+  }); 
+}
   
 }
--- src/app/servicios/portfolio.service.ts
+++ src/app/servicios/portfolio.service.ts
@@ -6,49 +6,58 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PortfolioService {
-  url: string = 'http://localhost:8080/ver/persona/9';
-  url2: string = 'http://localhost:8080';
+  url: string = 'http://localhost:8080';
 
   constructor(private http:HttpClient) { };
 
   obtenerDatos():Observable<any>{
-    return this.http.get(this.url);
+    return this.http.get(this.url + "/ver/persona/9");
+  }
+
+  /* Presentación */
+
+  buscarPres(id:any):Observable<any>{
+    return this.http.get(this.url + "/ver/persona/" + id)
+  }
+
+  modificarPres(miPortfolio: any):Observable<any>{
+    return this.http.put(this.url + "/change/persona", miPortfolio)
   }
 
    /* Experiencia */
 
   nuevaExp(experiencia:any, id:any):Observable<any>{
-    return this.http.post(this.url2 + "/new/experiencia/" + id, experiencia); 
+    return this.http.post(this.url + "/new/experiencia/" + id, experiencia); 
   }
 
   modificarExp(experiencia: any):Observable<any>{
-    return this.http.put(this.url2 + "/change/experiencia", experiencia);
+    return this.http.put(this.url + "/change/experiencia", experiencia);
   }
 
   buscarExp(id:any):Observable<any>{
-    return this.http.get(this.url2 + "/buscar /experiencia/"+ id);
+    return this.http.get(this.url + "/buscar /experiencia/"+ id);
   }
 
   borrarExp(id:any):Observable<any>{
-    return this.http.delete(this.url2 + "/delete/experiencia/" + id);
+    return this.http.delete(this.url + "/delete/experiencia/" + id);
   }
 
   /* Estudios */
 
   nuevoEst(estudios:any, id:any):Observable<any>{
-    return this.http.post(this.url2 + "/new/estudio/" + id, estudios);
+    return this.http.post(this.url + "/new/estudio/" + id, estudios);
   }
 
   modificarEst(estudio: any): Observable<any>{
-    return this.http.put(this.url2 + "/change/estudio", estudio);
+    return this.http.put(this.url + "/change/estudio", estudio);
   }
 
   buscarEst(id:any):Observable<any>{
-    return this.http.get(this.url2 + "/buscar /estudio/" + id)
+    return this.http.get(this.url + "/buscar /estudio/" + id)
   }
 
   borrarEst(id:any):Observable<any>{
-    return this.http.delete(this.url2 + "/delete/estudio/" + id)
+    return this.http.delete(this.url + "/delete/estudio/" + id)
   }
 
 } 
